1f322d24effaeb4a86e735bd6f58e24b587ecbd7
Worapoj Kreesuradej <worapoj@it.kmitl.ac.th>
2018-08-21 23:17:25 +0700

    Setup Start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express =require('express');
 const fs =require('fs');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -62,6 +63,6 @@ app.get('/bad', (req, res)=>{
     });
 }); 
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+    console.log('Server is up on port '+ port);
 });
